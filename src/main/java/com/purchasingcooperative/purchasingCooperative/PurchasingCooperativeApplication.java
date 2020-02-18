e66c931af84aabbeceffa91431ddf014b262fb9a
Kamil Ochnik <ilmak@o2.pl>
2020-02-18 12:25:48 +0100

    initial commit

---
+++ src/main/java/com/purchasingcooperative/purchasingCooperative/PurchasingCooperativeApplication.java
@@ -0,0 +1,13 @@
+package com.purchasingcooperative.purchasingCooperative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchasingCooperativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchasingCooperativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purchasingcooperative/purchasingCooperative/PurchasingCooperativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purchasingcooperative.purchasingCooperative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasingCooperativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
