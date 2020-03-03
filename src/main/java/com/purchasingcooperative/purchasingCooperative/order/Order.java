266bea55ef11635bc3089f7c0192ee4afc28a4cd
Kamil Ochnik <ilmak@o2.pl>
2020-03-03 12:54:29 +0100

    added order (in work)

---
+++ src/main/java/com/purchasingcooperative/purchasingCooperative/order/Order.java
@@ -0,0 +1,14 @@
+package com.purchasingcooperative.purchasingCooperative.order;
+
+import com.purchasingcooperative.purchasingCooperative.basket.Basket;
+
+public class Order {
+
+    private long id;
+
+    private OrderStatus orderStatus;
+
+    private long customerId;
+
+    private Basket basket;
+}
---
+++ src/main/java/com/purchasingcooperative/purchasingCooperative/order/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.purchasingcooperative.purchasingCooperative.order;
+
+public enum OrderStatus {
+    ACCEPTED, PAID, COMPLETING, PREPARED, RECEIVED, COMPLAINT, DONE, CANCELED
+}
